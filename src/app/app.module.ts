fecb10a0bd6ae0a04eb8de008a4822efb7659eee
Remko Boschker <remko@informatietuin.nl>
2017-05-16 14:36:19 +0200

    add material and fire-db

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,11 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
 import { AppComponent } from './app.component';
 import { environment } from "environments/environment";
+import { MdToolbarModule } from '@angular/material';
 
 @NgModule({
   declarations: [
@@ -14,7 +17,10 @@ import { environment } from "environments/environment";
     BrowserModule,
     FormsModule,
     HttpModule,
-    AngularFireModule.initializeApp(environment.firebase)
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireAuthModule,
+    AngularFireDatabaseModule,
+    MdToolbarModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
