1ee9b96af053b178a1a03589e9ce647e30d7be4b
Remko Boschker <remko@informatietuin.nl>
2017-05-16 11:34:18 +0200

    setup firebase deployment

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
+import { AngularFireModule } from 'angularfire2';
 import { AppComponent } from './app.component';
+import { environment } from "environments/environment";
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AngularFireModule.initializeApp(environment.firebase)
   ],
   providers: [],
   bootstrap: [AppComponent]
