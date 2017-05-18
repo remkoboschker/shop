fca5ef40b7b9812511ff24a02cb32f295288211a
Remko Boschker <remko@informatietuin.nl>
2017-05-18 23:45:57 +0200

    refactor auth service; need to include roles

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -2,27 +2,19 @@ import { CanActivate, Router, ActivatedRouteSnapshot, RouterStateSnapshot } from
 import { AngularFireAuth } from "angularfire2/auth";
 import { Injectable } from "@angular/core";
 import { Observable } from "rxjs/Rx";
+import * as firebase from 'firebase/app';
 import 'rxjs/add/operator/do';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/take';
 
 @Injectable()
-export class AuthService implements CanActivate {
-  constructor(private auth: AngularFireAuth, private router: Router) {}
-  
-  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | Observable<boolean> | Promise<boolean> {
-    return Observable.from(this.auth.authState)
-      .take(1)
-      .map(state => !!state)
-      .do(authenticated => {
-        if (!authenticated) {
-          this.router.navigateByUrl('/login');
-        }
-      });
+export class AuthService {
+
+  user: Observable<firebase.User>;
+  isAuthenticated: boolean;
+
+  constructor(private auth: AngularFireAuth, private router: Router) {
+    this.user = auth.authState;
+    this.user.subscribe(auth => { this.isAuthenticated = !!auth });
   }
-
-  
-
-
-
 }
